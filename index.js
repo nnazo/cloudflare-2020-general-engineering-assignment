d7d13c6851d5c7219a69432e9a2b0efd301a54d0
nnazo <jacobcurtis2266@gmail.com>
2020-10-10 12:40:33 -0500

    add links array

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+const links = [
+  { "name": "Cloudflare", "url": "https://cloudflare.com" },
+  { "name": "GitHub", "url": "https://github.com" },
+  { "name": "Rust", "url": "https://rust-lang.org" },
+  { "name": "Docker", "url": "https://docker.com" },
+  { "name": "DuckDuckGo", "url": "https://duckduckgo.com" },
+  { "name": "DigitalOcean", "url": "https://digitalocean.com" }
+];
+
 /**
  * Respond with hello worker text
  * @param {Request} request
